8a7512bc0f412bfd98f6fcf8cfabd57731519c68
PiotrPustul <piotrpustul@protonmail.ch>
2023-10-04 13:29:28 +0100

    modified app.js

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
 
